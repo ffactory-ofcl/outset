507940971d047b5cdb3a7b2da8dcc383d96117b7
Shane Galvin <sgalvinnc@gmail.com>
2017-11-01 16:08:36 -0400

    images preload before transition

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 Outset
 ======
 
-This web app uses the http://unsplash.it API to grab random images from http://unsplash.com and display them in a slideshow format on the users screen. 
+This web app uses the http://unsplash.it API to grab random images from http://unsplash.com and display them in a slideshow format on the users screen.
 
 It works purely on javascript and it should work on a majority of modern browsers.
 
-Currently allows the user to change the speed at which the images will change/transition and allows the user to blur the image with the API.
+Currently allows the user to change the frequency at which the images will change and allows the user to blur the image through the API.
 
 Feel free to fork it and make it better.
 
-Copyright
-======
+TODO
 
-Copyright 2014 Raajit Sharma
+[ ] Add crossfade
+
+
+
+
+Original author Raajit Sharma (2014)
--- js/main.js
+++ js/main.js
@@ -2,53 +2,56 @@
 var pwidth;
 var pheight;
 var backgroundCount = 0;
-var timer = 20000;
+var timer = 60; //seconds
+
+var newImage = new Image();
+
+
 
 
 
 //when the document has loaded
 $(document).ready(function(){
-	
+
 	//some preconfig, getting the browser container details also setting the fadingout the options
 	//also setting the timer.
-	$(".topOver").fadeOut();	
-	document.getElementById('timer').value = timer/1000;	
+	$(".topOver").fadeOut();
+	$("#mainImage").fadeIn(100);
+	document.getElementById('timer').value = timer;
 	pwidth = $(window).width();
 	pheight = $(window).height();
 	$('#allContent').mouseleave(showDetailsEnd);
 
-	
-	
+
+
 	//the timer
 	changeImage();
-	
+
 	//when the window is resized, it will update the width and height
 	$(window).resize(function(){
-		pwidth = $(window).width(); 
+		pwidth = $(window).width();
 		pheight = $(window).height();
 	});
 });
 
-//function for changing the image to the next one, including the new dimensions
-//also includes fadeIn and fadeOut animations
+// stert loading the new image
 function changeImage()
 {
-
-	$("#mainImage").fadeOut();
-	
-	//check if the blur checkbox is ticked
-	if (document.getElementById('blur').checked) {
-        $('#mainImage').attr('src', 'https://unsplash.it/' + pwidth + '/' + pheight + '/?random&blur&' + backgroundCount);
-    } else {
-		$('#mainImage').attr('src', 'https://unsplash.it/' + pwidth + '/' + pheight + '/?random&' + backgroundCount);
-	}
-	
-	//when image loaded, start the timer and show image
-	$('#mainImage').on('load', function(){
-		$("#mainImage").fadeIn();
-		restartTimer();
-	});
+	var blurStr = (document.getElementById('blur').checked) ? '&blur' : '';
+	newImage.src = 'https://unsplash.it/' + pwidth + '/' + pheight + '/?random' + blurStr + '&' + backgroundCount;
 	backgroundCount++;
+}
+
+// now that the new image is loaded, fade out the current one
+newImage.onload = function() {
+	$("#mainImage").fadeOut(1000, switchPic);
+
+}
+
+// now that the current image has been faded out, load in the new one
+function switchPic(){
+	$("#mainImage").attr('src', newImage.src);
+	$("#mainImage").fadeIn(1000, restartTimer);
 }
 
 //when hovering over an image show the details.
@@ -60,21 +63,30 @@ function showDetails()
 
 //when not hovering over an image hide details.
 function showDetailsEnd()
-{	
+{
 	$(".topOver").fadeOut();
 }
 
 //when the timer is changed, set the new timer variable to what the user has entered, no input validation though
 function setTimer()
-{	
+{
 	var value = document.getElementById('timer').value;
-	timer = value * 1000;
+	timer = value;
 }
 
 //basically to start the timer for the clock
 function restartTimer()
 {
 	timePrompt = setTimeout(function(){
-		changeImage();	
-	}, timer);
+		changeImage();
+	}, timer*1000);
+}
+
+function imageClicked()
+{
+	// prevent race condition. 60 second timeout
+	timePrompt = setTimeout(function(){
+		changeImage();
+	}, 60*1000);
+	changeImage();
 }
